4e8a5482a0795ba087895f9e0e6ca8c4e942e31b
hambre <hotheating@gmail.com>
2019-08-02 17:42:06 +0900

    Initial commit

---
+++ app/src/main/java/com/example/itwill/myapplication190802_04/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.itwill.myapplication190802_04;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.item1:
+                return true;
+        }
+        return false;
+    }
+}
